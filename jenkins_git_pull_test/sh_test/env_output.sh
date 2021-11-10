9b52a1d1dce14539b0efe338ed7867014102b29d
duweizhi <duweizhi@cokutau.com>
2021-11-10 12:06:31 +0800

    change some script

--- jenkins_git_pull_test/sh_test/env_output.sh
+++ jenkins_git_pull_test/sh_test/env_output.sh
@@ -2,16 +2,25 @@
 
 eval $(/usr/bin/ssh-agent -s)
 ssh_passphrase=linux,123
+#E:\github\some_boring_tools\jenkins_git_pull_test\sh_test
+
 ssh_key_path=/e/github/gitee_key/gitlab_test
-ssh_passphrase_path=/e/github/sh_test/ssh_pass
-echo ${ssh_passphrase}
-echo "echo ${ssh_passphrase}" > ${ssh_passphrase_path}
-chmod +x ${ssh_passphrase_path}
-echo ${ssh_key_path}
+#ssh_passphrase_path=/e/github/some_boring_tools/jenkins_git_pull_test/sh_test/ssh_pass
+ssh_passphrase_name=SSH_PASS
+shell_output_json=git_bash_output.json
+work_path=$(cd "$(dirname $0)";pwd)
+cd ${work_path}
+
+
+
+#echo ${ssh_passphrase}
+echo "echo ${ssh_passphrase}" > ${ssh_passphrase_name}
+chmod +x ${ssh_passphrase_name}
+#echo ${ssh_key_path}
 
 # https://unix.stackexchange.com/questions/571741/how-to-pass-a-passphrase-to-ssh-add-without-triggering-a-prompt
 # 直接把密码传到密钥，不需要使用except模块进行自动交互
-DISPLAY=1 SSH_ASKPASS="${ssh_passphrase_path}" ssh-add ${ssh_key_path} < /dev/null
+DISPLAY=1 SSH_ASKPASS="${ssh_passphrase_name}" ssh-add ${ssh_key_path} < /dev/null
 ssh-add -l
 
 env_var=(SSH_AGENT_PID SSH_AUTH_SOCK)
@@ -25,7 +34,7 @@ done
 
 
 
-output_fun(){
+output_func(){
 	printf "{\n"
 	printf '\t"data":[\n'
 	#for ((i=0;i<${#output_dict[@]};i++))
@@ -39,8 +48,4 @@ output_fun(){
 
 }
 
-test1(){
-	echo "this is 1"
-}
-
-output_fun  > /e/github/sh_test/1231234_output
+output_func  > ${shell_output_json}
